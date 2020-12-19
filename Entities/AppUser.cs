51fa817fcc6c8b6367b7bd555e28af992d74f97b
Pau Camós Parra <pau.camos@apartool.com>
2020-12-19 15:07:07 +0100

    Added DB Data and Connection

--- Entities/AppUser.cs
+++ Entities/AppUser.cs
@@ -8,5 +8,17 @@ namespace API.Entities
     public class AppUser
     {
         public int Id { get; set; }
+        public string UserName { get; set; }
+
+    /* Full Definition Properties
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+    */
+
     }
 }
