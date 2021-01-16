902f1b11067fc609aa690ca218b303b08626e397
Pau Camós Parra <pau.camos@apartool.com>
2021-01-16 13:06:36 +0100

    Cambios

--- Controllers/BaseApiController.cs
+++ Controllers/BaseApiController.cs
@@ -8,7 +8,7 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class BaseApiController : Controller
+    public class BaseApiController : ControllerBase
     {
 
     }
